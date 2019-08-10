c946ab4e390ec0844451a84962e001b91e1acd40
Kom Sihon <rsihon@gmail.com>
2019-08-10 20:03:36 +0100

    Permission names written in a gettext to cause them to appear in translation file

--- foulassi/scripts.py
+++ foulassi/scripts.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import gettext as _
 from ikwen_foulassi.foulassi.models import Student, Teacher, Event
 from ikwen_foulassi.school.models import Level, Score
 from ikwen_foulassi.reporting.models import SessionReport, ReportCardBatch
@@ -7,16 +8,16 @@ from ikwen_foulassi.reporting.models import SessionReport, ReportCardBatch
 
 def create_permissions():
     student_ct = ContentType.objects.get_for_model(Student)
-    Permission.objects.create(codename='ik_manage_student', name='Access students info', content_type=student_ct)
+    Permission.objects.create(codename='ik_manage_student', name=_('Access students info'), content_type=student_ct)
     teacher_ct = ContentType.objects.get_for_model(Teacher)
-    Permission.objects.create(codename='ik_manage_teacher', name='Manage teachers', content_type=teacher_ct)
+    Permission.objects.create(codename='ik_manage_teacher', name=_('Manage teachers'), content_type=teacher_ct)
     level_ct = ContentType.objects.get_for_model(Level)
-    Permission.objects.create(codename='ik_manage_school', name='Manage school setup', content_type=level_ct)
+    Permission.objects.create(codename='ik_manage_school', name=_('Manage school setup'), content_type=level_ct)
     score_ct = ContentType.objects.get_for_model(Score)
-    Permission.objects.create(codename='ik_access_scores', name='Access student scores', content_type=score_ct)
+    Permission.objects.create(codename='ik_access_scores', name=_('Access student scores'), content_type=score_ct)
     report_ct = ContentType.objects.get_for_model(SessionReport)
-    Permission.objects.create(codename='ik_view_dashboard', name='View dashboard', content_type=report_ct)
+    Permission.objects.create(codename='ik_view_dashboard', name=_('View dashboard'), content_type=report_ct)
     batch_ct = ContentType.objects.get_for_model(ReportCardBatch)
-    Permission.objects.create(codename='ik_manage_reporting', name='Manage report cards', content_type=batch_ct)
+    Permission.objects.create(codename='ik_manage_reporting', name=_('Manage report cards'), content_type=batch_ct)
     event_ct = ContentType.objects.get_for_model(Event)
-    Permission.objects.create(codename='ik_view_event', name='View events', content_type=event_ct)
+    Permission.objects.create(codename='ik_view_event', name=_('View events'), content_type=event_ct)
