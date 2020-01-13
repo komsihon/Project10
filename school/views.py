ee4ba998188e189205b289daeeb12aca7ec7cd88
Kom Sihon <rsihon@gmail.com>
2020-01-13 14:17:10 +0100

    1 - Tabs of TabView now swipe on all screen sizes 2 - Updated translations

--- school/views.py
+++ school/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template.defaultfilters import slugify
 from django.utils.decorators import method_decorator
-from django.utils.translation import gettext as _
+from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import TemplateView
