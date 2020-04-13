c142b8068ec04d9382e24163a00423cc08b52acc
theYogam <44173266+theyogam@users.noreply.github.com>
2020-04-13 10:26:36 +0100

    Minor change on install.js

--- static/foulassi/install.js
+++ static/foulassi/install.js
@@ -34,7 +34,7 @@ function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
     deferredInstallPrompt = evt;
     installButton.removeAttribute('hidden');
-    document.getElementById('install-overlay').remove("hidden");
+    document.getElementById('install-overlay').removeAttribute("hidden");
 }
 
 
